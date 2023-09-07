9f8850e2ed8b1c70f1b41bc50bdfd09a225b2109
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2023-09-07 13:42:36 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Clever Programmer</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// 'https://api.themoviedb.org/3/movie/11?api_key=50832de084e907f7283dcc6fc83e3fec'
+// 50832de084e907f7283dcc6fc83e3fec
